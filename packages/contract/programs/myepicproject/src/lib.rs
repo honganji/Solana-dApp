d479b615e7c975cf87d27511d5060eb1733b2c7b
github honganji <yujicavs@icloud.com>
2023-06-05 11:20:25 +0900

    complete contract

--- packages/contract/programs/myepicproject/src/lib.rs
+++ packages/contract/programs/myepicproject/src/lib.rs
@@ -5,11 +5,58 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod myepicproject {
     use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // add_gif関数はgif_linkを受け取れるようになりました。
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // gif_linkとuser_addressを格納するための構造体を作成します。
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        // gif_listにitemを追加します。
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// AddGifメソッドを呼び出した署名者を構造体に追加し、保存できるようにします。
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// カスタム構造体を作成します。
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // ItemStruct型のVectorをアカウントにアタッチします。
+    pub gif_list: Vec<ItemStruct>,
+}
